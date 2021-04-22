afe8a8d6e94263aa7168648c089ac0c8122e0c19
Vadym Panchenko <vadymp@default-value.com>
2021-04-22 13:15:39 +0300

    Added mobile and desktop styles for section 'contact-us'

--- pub/js/main.js
+++ pub/js/main.js
@@ -2,7 +2,6 @@ var $ = require("jquery");
 
 $(document).ready(function () {
     $('.icon-container').click(function () {
-        console.log('Click test!!!');
         $(document.body).toggleClass('menu-open');
         $('.mobile-menu').toggleClass('menu-open');
     });
